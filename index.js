d8a532050a4dcae3833613990d84e97e365e9fe0
Ano2225 <ouatt0767@gmail.com>
2023-11-21 10:22:04 +0000

    First commit , my work in 1h45

---
+++ index.js
@@ -0,0 +1,6 @@
+   // Afficher le contenu apres 3s
+
+   setTimeout(function() {
+    document.getElementById('loader').style.display ='none'
+    document.getElementById('content').style.display='block'
+}, 2000)
